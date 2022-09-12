33bc82bcd7a86ade15c8641e1521670ebab2539d
Sukmaaaa <9sukma@gmail.com>
2022-09-12 13:28:17 +0700

    senin 12 sep

--- resources/views/Series/create.blade.php
+++ resources/views/Series/create.blade.php
@@ -21,13 +21,11 @@
         <div class="row">
         <x-adminlte-input type="text" name="title" label="Title:" placeholder="Season 1" fgroup-class="col-md-6">
             </x-adminlte-input>
-
         <x-adminlte-input type="url" name="cover" label="Cover:" placeholder="Please insert link" fgroup-class="col-md-6">
         </x-adminlte-input>
         </div>
 
         <div class="row">
-            
             <x-adminlte-textarea type="text" name="article" label="Article :" placeholder="The first season of the Attack on Titan anime television series was produced by IG Port's Wit Studio and directed by Tetsurō Araki, with Yasuko Kobayashi handling series composition and Kyōji Asano providing character designs" fgroup-class="col-md-12">
             </x-adminlte-textarea> 
         </div>
@@ -42,15 +40,31 @@
         </div>
 
         <div class="row">
-            <x-adminlte-input type="date" name="originalRelease" label="Original Release:" fgroup-class="col-md-6">
+            <x-adminlte-input type="date" name="originalRelease" label="Original Release:" fgroup-class="col-12">
             </x-adminlte-input>
-            <x-adminlte-select name="genre" label="Genre:" fgroup-class="col-md-6">
-                <option value="Action">Action</option>
-                <option value="Dark Fantasy">Dark Fantasy</option>
-                <option value="Post Apocalyptic">Post Apocalyptic</option>
-                <option value="Comedy">Comedy</option>
-                <option value="Romance">Romance</option>
-            </x-adminlte-select>
+            <div class="col">
+                <label for="genreLabel">Genre:</label>
+                <div class="col icheck-primary">
+                    <input type="checkbox" id="someCheckboxId" name="genre[]" value="action"/>
+                     <label for="someCheckboxId">Action</label>
+                </div>
+                <div class="col icheck-primary">
+                    <input type="checkbox" id="someCheckboxId" name="genre[]" value="dark-fantasy"/>
+                     <label for="someCheckboxId">Dark Fantasy</label>
+                </div>
+                <div class="col icheck-primary">
+                    <input type="checkbox" id="someCheckboxId" name="genre[]" value="post-apocalyptic"/>
+                     <label for="someCheckboxId">Post Apocalyptic</label>
+                </div>
+                <div class="col icheck-primary">
+                    <input type="checkbox" id="someCheckboxId" name="genre[]" value="comedy" />
+                     <label for="someCheckboxId">Comedy</label>
+                </div>
+                <div class="col icheck-primary">
+                    <input type="checkbox" id="someCheckboxId" name="genre[]" value="romance" />
+                     <label for="someCheckboxId">Romance</label>
+                </div>
+            </div>
         </div>
         
         <div class="d-flex flex-row justify-content-between">
